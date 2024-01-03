477eb019876d12b0c76d4af4d8c4a2a5e5438cb4
Timoj <yabokutv@web.de>
2024-01-03 17:11:39 +0100

    adjusted player prefab and spawner

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -9,16 +9,23 @@ public class SpawnPlayers : MonoBehaviour
     //public PUN2_GameLobby gamelobby;
 
     public GameObject playerPrefab;
+    public GameObject playerPrefab2;
 
     public float minX;
     public float maxX;
     public float minZ;
     public float maxZ;
+
+    public Vector3 playerSpawnPosition1 = new Vector3(-3, 1, 5);
+    public Vector3 playerSpawnPosition2 = new Vector3(3, 1, 5);
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), -15.07465f, Random.Range(minZ, maxZ));
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        //Vector3 randomPosition = new Vector3(Random.Range(minX, maxX), -15.07465f, Random.Range(minZ, maxZ));
+        //PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        PhotonNetwork.Instantiate(playerPrefab.name, playerSpawnPosition1, Quaternion.identity);
+        PhotonNetwork.Instantiate(playerPrefab2.name, playerSpawnPosition2, Quaternion.identity);
 
         //gamelobby = FindAnyObjectByType<PUN2_GameLobby>();
         //if(gamelobby.joiningRoom)
