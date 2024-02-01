278a62212dc5a5b70822f1d5602c7fc1764bc42e
Robert Berg <r.berg.2001@gmail.com>
2024-02-01 17:03:26 +0100

    add magnetism

---
+++ Assets/Scripts/MagnetismEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MagnetismEffect : MonoBehaviour
+{
+    public string targetTag = "magnetic";
+    public float pullRadius = 5f;
+    public float pullForce = 100f;
+
+    void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, pullRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag(targetTag))
+            {
+                Debug.Log("Target within radius. Applying force.");
+                Vector3 pullDirection = (collider.transform.position - transform.position).normalized;
+                float distance = Vector3.Distance(collider.transform.position, transform.position);
+                float newPullForce = Mathf.Lerp(pullForce, 0, distance / pullRadius);
+                GetComponent<Rigidbody>().AddForce(pullDirection * newPullForce);
+            }
+        }
+    }
+}
